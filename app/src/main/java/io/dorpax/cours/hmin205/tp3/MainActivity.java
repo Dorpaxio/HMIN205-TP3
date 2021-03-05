f60bf560ecf0e4b6bb5900e1e64aab3efd29895d
Charles DESGENETEZ <dorpaxione@gmail.com>
2021-03-05 15:51:47 +0100

    first commit

---
+++ app/src/main/java/io/dorpax/cours/hmin205/tp3/MainActivity.java
@@ -0,0 +1,27 @@
+package io.dorpax.cours.hmin205.tp3;
+
+import android.os.PersistableBundle;
+import android.widget.EditText;
+import android.widget.Toast;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+    }
+}
